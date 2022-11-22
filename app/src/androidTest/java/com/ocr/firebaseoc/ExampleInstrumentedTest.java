855ee4b30334ee7a1df096f5feef9d89078beb0e
ouardamerabet <ouardamerabet@3s-pay.com>
2022-11-22 11:09:37 +0100

    mail authentification

--- app/src/androidTest/java/com/ocr/firebaseoc/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/ocr/firebaseoc/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.openclassrooms.firebaseoc", appContext.getPackageName());
+        assertEquals("com.ocr.firebaseoc", appContext.getPackageName());
     }
 }
--- app/src/main/java/com/ocr/firebaseoc/MainActivity.java
+++ app/src/main/java/com/ocr/firebaseoc/MainActivity.java
@@ -2,11 +2,21 @@ package com.ocr.firebaseoc;
 
 import androidx.annotation.Nullable;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
 import com.ocr.firebaseoc.databinding.ActivityMainBinding;
 
+import java.util.Collections;
+import java.util.List;
+
 public class MainActivity extends BaseActivity<ActivityMainBinding> {
+
+    private static final int RC_SIGN_IN =123 ;
 
     @Override
     ActivityMainBinding getViewBinding() {
@@ -16,6 +26,62 @@ public class MainActivity extends BaseActivity<ActivityMainBinding> {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setupListeners();
+    }
+    private void setupListeners(){
+        // Login Button
+        binding.loginButton.setOnClickListener(view -> {
+            startSignInActivity();
+        });
+    }
+
+    private void startSignInActivity(){
+
+        // Choose authentication providers
+        List<AuthUI.IdpConfig> providers =
+                Collections.singletonList(new AuthUI.IdpConfig.EmailBuilder().build());
+
+        // Launch the activity
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setTheme(R.style.LoginTheme)
+                        .setAvailableProviders(providers)
+                        .setIsSmartLockEnabled(false, true)
+                        .setLogo(R.drawable.ic_logo_auth)
+                        .build(),
+                RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        this.handleResponseAfterSignIn(requestCode, resultCode, data);
+    }
+
+    // Method that handles response after SignIn Activity close
+    private void handleResponseAfterSignIn(int requestCode, int resultCode, Intent data){
+
+        IdpResponse response = IdpResponse.fromResultIntent(data);
+
+        if (requestCode == RC_SIGN_IN) {
+            // SUCCESS
+            if (resultCode == RESULT_OK) {
+                //userManager.createUser();
+                Toast.makeText(this,getString(R.string.connection_succeed),Toast.LENGTH_SHORT).show();
+            } else {
+                // ERRORS
+                if (response == null) {
+                    Toast.makeText(this,getString(R.string.error_authentication_canceled),Toast.LENGTH_SHORT).show();
+                } else if (response.getError()!= null) {
+                    if(response.getError().getErrorCode() == ErrorCodes.NO_NETWORK){
+                        Toast.makeText(this,getString(R.string.error_no_internet),Toast.LENGTH_SHORT).show();
+                    } else if (response.getError().getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
+                        Toast.makeText(this,getString(R.string.error_unknown_error),Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        }
     }
 
 }
